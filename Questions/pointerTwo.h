966a6b9cd8fc8c19fa54f88bb121ba642712e32f
chrishoran8 <horan_chris@hotmail.co.uk>
2021-03-03 16:01:05 +0000

    made changes

--- Questions/pointerTwo.h
+++ Questions/pointerTwo.h
@@ -3,12 +3,25 @@
 #include "pointerOne.h"
 using namespace std;
 
-int fun(int t[]) {
-	t[0] += t[1];
-	return t[0];
+struct s {
+	float* f;
+};
+
+void make(s* p, float x = 10) { // a pointer of ss is taken in here of type s called p
+	float* f = new float; //a new null pointer of type f is created here	
+	*f = sizeof(*f) / sizeof(float) * 10; // the size of *f and the size of float are the same
+	//*f = 4 / 4 * 10 = 10
+	
+	p->f = f; //p.f = 10
+	cout << "f: " << *f << endl; // is a memory location
+	//object pointer p.f = memory location of above f
 }
 
-int mainMethodTwo() {
-	cout << "main method 2 changed on laptop";
-	return 0;
+void mainMethodTwo() {
+	s* ss = new s; //make a new pointer of type s called ss
+	make(ss); // pass the pointer of ss to the make function
+	cout << *(*ss).f; // the value (the above pointer).f
+	delete ss->f;
+	delete ss;
+
 }
